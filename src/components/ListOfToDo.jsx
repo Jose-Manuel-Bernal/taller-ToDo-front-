a8c137755c83a3ea5fb9b2e0a9e8e69aa68b63e1
Jose Manuel Bernal Aguilar <josem.bernal@udea.edu.co>
2022-05-04 21:31:33 -0500

    added the 'delete' buttom, and the final functions

--- src/components/ListOfToDo.jsx
+++ src/components/ListOfToDo.jsx
@@ -4,6 +4,21 @@ import { Store } from "./StoreProvider";
 const ListOfToDo = () => {
   const { state, dispatch } = useContext(Store);
 
+  const onCheckbox = (event, note) => {
+    const checked = event.currentTarget.checked;
+    dispatch({
+      type: "update-note",
+      payload: { ...note, done: checked },
+    });
+  };
+
+  const onDelete = (note) => {
+    dispatch({
+      type: "remove-note",
+      payload: note,
+    });
+  };
+
   return (
     <div>
       <h1>Actions pending to be done</h1>
@@ -16,6 +31,12 @@ const ListOfToDo = () => {
             >
               {note.title} <br />
               {note.message} <br />
+              <input
+                onChange={(event) => onCheckbox(event, note)}
+                type="checkbox"
+                checked={note.done}
+              />
+              <button onClick={() => onDelete(note)}>Delete</button>
             </li>
           );
         })}
